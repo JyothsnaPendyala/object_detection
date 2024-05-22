be1d3dac92177429783f6d7dc384a7b821739614
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-05-22 16:03:36 +0530

    Update data_transformation.py

--- data_transformation.py
+++ data_transformation.py
@@ -13,61 +13,7 @@ from albumentations.pytorch import ToTensorV2
 from torchvision import transforms as transforms
 import pickle
 
-def transform_data():
-    # Define the training tranforms
-    def get_train_aug():
-        return A.Compose([
-            A.MotionBlur(blur_limit=3, p=0.5),
-            A.Blur(blur_limit=3, p=0.5),
-            A.RandomBrightnessContrast(
-                brightness_limit=0.2, p=0.5
-            ),
-            A.ColorJitter(p=0.5),
-            # A.Rotate(limit=10, p=0.2),
-            A.RandomGamma(p=0.2),
-            A.RandomFog(p=0.2),
-            # A.RandomSunFlare(p=0.1),
-            # `RandomScale` for multi-res training,
-            # `scale_factor` should not be too high, else may result in 
-            # negative convolutional dimensions.
-            # A.RandomScale(scale_limit=0.15, p=0.1),
-            # A.Normalize(
-            #     (0.485, 0.456, 0.406),
-            #     (0.229, 0.224, 0.225)
-            # ),
-            ToTensorV2(p=1.0),
-        ], bbox_params={
-            'format': 'pascal_voc',
-            'label_fields': ['labels']
-        })
-
-    def get_train_transform():
-        return A.Compose([
-            # A.Normalize(
-            #     (0.485, 0.456, 0.406),
-            #     (0.229, 0.224, 0.225)
-            # ),
-            ToTensorV2(p=1.0),
-        ], bbox_params={
-            'format': 'pascal_voc',
-            'label_fields': ['labels']
-        })
-
-    # Define the validation transforms
-    def get_valid_transform():
-        return A.Compose([
-            # A.Normalize(
-            #     (0.485, 0.456, 0.406),
-            #     (0.229, 0.224, 0.225)
-            # ),
-            ToTensorV2(p=1.0),
-        ], bbox_params={
-            'format': 'pascal_voc', 
-            'label_fields': ['labels']
-        })
-
-    global CustomDataset
-    class CustomDataset(Dataset):
+class CustomDataset(Dataset):
         def __init__(
             self, images_path, labels_path, 
             width, height, classes, transforms=None, 
@@ -323,6 +269,59 @@ def transform_data():
             
         def __len__(self):
             return len(self.all_images)
+
+def transform_data():
+    # Define the training tranforms
+    def get_train_aug():
+        return A.Compose([
+            A.MotionBlur(blur_limit=3, p=0.5),
+            A.Blur(blur_limit=3, p=0.5),
+            A.RandomBrightnessContrast(
+                brightness_limit=0.2, p=0.5
+            ),
+            A.ColorJitter(p=0.5),
+            # A.Rotate(limit=10, p=0.2),
+            A.RandomGamma(p=0.2),
+            A.RandomFog(p=0.2),
+            # A.RandomSunFlare(p=0.1),
+            # `RandomScale` for multi-res training,
+            # `scale_factor` should not be too high, else may result in 
+            # negative convolutional dimensions.
+            # A.RandomScale(scale_limit=0.15, p=0.1),
+            # A.Normalize(
+            #     (0.485, 0.456, 0.406),
+            #     (0.229, 0.224, 0.225)
+            # ),
+            ToTensorV2(p=1.0),
+        ], bbox_params={
+            'format': 'pascal_voc',
+            'label_fields': ['labels']
+        })
+
+    def get_train_transform():
+        return A.Compose([
+            # A.Normalize(
+            #     (0.485, 0.456, 0.406),
+            #     (0.229, 0.224, 0.225)
+            # ),
+            ToTensorV2(p=1.0),
+        ], bbox_params={
+            'format': 'pascal_voc',
+            'label_fields': ['labels']
+        })
+
+    # Define the validation transforms
+    def get_valid_transform():
+        return A.Compose([
+            # A.Normalize(
+            #     (0.485, 0.456, 0.406),
+            #     (0.229, 0.224, 0.225)
+            # ),
+            ToTensorV2(p=1.0),
+        ], bbox_params={
+            'format': 'pascal_voc', 
+            'label_fields': ['labels']
+        })
 
     IMAGE_WIDTH = 640
     IMAGE_HEIGHT = 480
