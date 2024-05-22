9be21270c96cd80c2c6eba98044affec4bfe39b6
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-05-22 12:55:30 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -3,8 +3,9 @@ import os
 
 # os.environ["AWS_ACCESS_KEY_ID"]="your access key"
 # os.environ["AWS_SECRET_ACCESS_KEY"]="your secret key"
-
-print("----------:",printenv)
+aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
+# print("----------:",printenv)
 def extract_data():
 
     s3 = boto3.client('s3')
