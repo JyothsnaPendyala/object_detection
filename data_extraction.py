b3e86705d87e35f07001fcde474f23d17d91f581
JyothsnaPendyala <100350083+JyothsnaPendyala@users.noreply.github.com>
2024-12-04 14:54:54 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -6,10 +6,25 @@ import os
 aws_access_key_id = os.environ.get('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.environ.get('AWS_SECRET_ACCESS_KEY')
 # print("----------:",printenv)
-def extract_data():
 
+# def extract_data():
+
+#     s3 = boto3.client('s3')
+#     bucket_name = 'deeplearning-mlops'
+#     url = s3.generate_presigned_url(
+#                     ClientMethod='get_object',
+#                     Params={'Bucket': bucket_name, 'Key': 'object_detection_data.zip'},
+#                     ExpiresIn=7200  # URL expiration time in seconds (adjust as needed)
+#                 )
+#     print(url)
+#     return url
+
+# extract_data()
+
+def extract_data():
+ 
     s3 = boto3.client('s3')
-    bucket_name = 'deeplearning-mlops'
+    bucket_name = 'deeplearning-mlops-demo'
     url = s3.generate_presigned_url(
                     ClientMethod='get_object',
                     Params={'Bucket': bucket_name, 'Key': 'object_detection_data.zip'},
@@ -17,5 +32,5 @@ def extract_data():
                 )
     print(url)
     return url
-
+ 
 extract_data()
